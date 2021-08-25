75762f927dfa9de22cc90d03ed6319a095afec5c
Huhui <wishgood@gmail.com>
2021-08-25 14:39:47 +0800

    fix: context

--- packages/flow-graph/src/shared/context.ts
+++ packages/flow-graph/src/shared/context.ts
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Flow } from '../models';
 
-export const FlowContext = React.createContext<Flow>(new Flow());
+export const FlowContext = React.createContext<Flow | undefined>(undefined);
