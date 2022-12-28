f49843867a9d51703f4633277eb732a79f95681f
王宇杰 <798032825@qq.com>
2022-12-28 16:37:23 +0800

    feat: main的panel组件封装

--- examples/toybox/playground/main.tsx
+++ examples/toybox/playground/main.tsx
@@ -27,19 +27,19 @@ GlobalRegistry.setDesignerLanguage('en-US')
 GlobalRegistry.registerDesignerIcons(icons)
 
 export const App: React.FC = () => {
-  const [leftVisible, setLeftVisible] = React.useState(false)
-  const [leftActiveKey, setLeftActiveKey] = React.useState()
-  const [rightVisible, setRightVisible] = React.useState(false)
-  const [rightActiveKey, setRightActiveKey] = React.useState()
-  const [errorData, setErrorData] = React.useState([])
-  const [warnData, setWarnData] = React.useState([])
+  // const [leftVisible, setLeftVisible] = React.useState(false)
+  // const [leftActiveKey, setLeftActiveKey] = React.useState()
+  // const [rightVisible, setRightVisible] = React.useState(false)
+  // const [rightActiveKey, setRightActiveKey] = React.useState()
+  // const [errorData, setErrorData] = React.useState([])
+  // const [warnData, setWarnData] = React.useState([])
   const eventEngine = new EventEngine()
   const metaFlow = new MetaFlow(FlowModeEnum.EDIT)
   const freeFlow = new FreeFlow(FlowModeEnum.EDIT)
   useEffect(() => {
-    freeFlow.setMetaFlow(freeInitMeta, 'AUTO_START_UP')
     metaFlow.setMetaFlow({}, 'AUTO_START_UP')
-  }, [metaFlow])
+    freeFlow.setMetaFlow(freeInitMeta, 'AUTO_START_UP')
+  }, [metaFlow, freeFlow])
   return (
     <div className="App">
       <FlowContext.Provider
@@ -51,92 +51,8 @@ export const App: React.FC = () => {
           eventEngine,
         }}
       >
-        {/* <Panel /> */}
+        <Panel />
         {/* <LeftPanel /> */}
-        <StudioPanel>
-          <TopbarPanel>
-            <TopbarPanel.Region position="left">
-              <CompositePanel
-                visible={leftVisible}
-                setVisible={setLeftVisible}
-                activeKey={leftActiveKey}
-                setActiveKey={setLeftActiveKey as any}
-              >
-                <CompositePanel.Item
-                  title="flowDesigner.panels.resource"
-                  icon="Add"
-                />
-              </CompositePanel>
-            </TopbarPanel.Region>
-            {/* <TopbarPanel.Region position="center">
-            <CompositePanel>
-              <CompositePanel.Item title="回退" onClick={back} icon="Undo" />
-              <CompositePanel.Item title="前进" onClick={next} icon="Redo" />
-            </CompositePanel>
-          </TopbarPanel.Region> */}
-            <TopbarPanel.Region position="right">
-              <CompositePanel
-                direction="right"
-                visible={rightVisible}
-                setVisible={setRightVisible}
-                activeKey={rightActiveKey}
-                setActiveKey={setRightActiveKey as any}
-              >
-                {/* <CompositePanel.Item
-                title="flowDesigner.panels.warn"
-                icon="Profile"
-                activeKey="warn"
-              /> */}
-                <CompositePanel.Item
-                  title="flowDesigner.panels.error"
-                  icon="Profile"
-                  activeKey="error"
-                />
-              </CompositePanel>
-            </TopbarPanel.Region>
-          </TopbarPanel>
-          <div
-            style={{
-              display: 'flex',
-              flexGrow: 1,
-              height: 'calc(100% - 48px)',
-            }}
-          >
-            <CompositePanelContent
-              activeKey={leftActiveKey}
-              visible={true}
-              onClose={() => setLeftVisible(false)}
-            >
-              <CompositePanelContent.Item title="flowDesigner.panels.element">
-                {/* <ResourceWidget flowGraph={flowGraph} /> */}
-                {/* <LeftPanel /> */}
-                <ResourceWidget
-                  title="flowDesigner.panels.sources.logical"
-                  sources={itemMap}
-                />
-              </CompositePanelContent.Item>
-            </CompositePanelContent>
-            <WorkspacePanel>
-              {/* <FlowEditor /> */}
-              <FlowCanvas />
-            </WorkspacePanel>
-            <CompositePanelContent
-              visible={rightVisible}
-              activeKey={rightActiveKey}
-              onClose={() => setRightVisible(false)}
-            >
-              {/* <CompositePanelContent.Item
-              title="flowDesigner.panels.warn"
-              activeKey="warn"
-            >
-              <WarnWidget dataList={warnData} />
-            </CompositePanelContent.Item> */}
-              <CompositePanelContent.Item title="flowDesigner.panels.error">
-                <ErrorWidget dataList={errorData} />
-              </CompositePanelContent.Item>
-            </CompositePanelContent>
-          </div>
-        </StudioPanel>
       </FlowContext.Provider>
     </div>
   )
--- examples/toybox/src/components/panel/index.tsx
+++ examples/toybox/src/components/panel/index.tsx
@@ -2,7 +2,21 @@ import React, { useCallback } from 'react'
 import { useMetaFlow, useFreeFlow } from '@toy-box/flow-node'
 import { flowData1, flowData2, flowMeta, freeMeta } from '../../data/flowData'
 import { LayoutModeEnum } from '@toy-box/flow-graph'
-
+import {
+  icons,
+  CompositePanel,
+  TopbarPanel,
+  CompositePanelContent,
+  StudioPanel,
+  WorkspacePanel,
+} from '@toy-box/studio-base'
+import {
+  FlowCanvas,
+  LeftPanel,
+  ErrorWidget,
+  ResourceWidget,
+} from '../../../src'
+import { itemMap } from '../../../src/data/itemMap'
 export const Panel = () => {
   const metaFlow = useMetaFlow()
   const freeFlow = useFreeFlow()
@@ -32,13 +46,103 @@ export const Panel = () => {
     // freeFlow.
     // freeFlow..layoutFlow()
   }, [])
+  const [leftVisible, setLeftVisible] = React.useState(false)
+  const [leftActiveKey, setLeftActiveKey] = React.useState()
+  const [rightVisible, setRightVisible] = React.useState(false)
+  const [rightActiveKey, setRightActiveKey] = React.useState()
+  const [errorData, setErrorData] = React.useState([])
+  const [warnData, setWarnData] = React.useState([])
   return (
-    <div>
-      <button onClick={init}>init</button>
-      <button onClick={update}>update</button>
-      <button onClick={handleMetaFlow}>metaflow</button>
-      <button onClick={handleFreeLayout}>freeLayout</button>
-      <button onClick={handleExport}>export</button>
-    </div>
+    // <div>
+    //   <button onClick={init}>init</button>
+    //   <button onClick={update}>update</button>
+    //   <button onClick={handleMetaFlow}>metaflow</button>
+    //   <button onClick={handleFreeLayout}>freeLayout</button>
+    //   <button onClick={handleExport}>export</button>
+    // </div>
+    <StudioPanel>
+      <TopbarPanel>
+        <TopbarPanel.Region position="left">
+          <CompositePanel
+            visible={leftVisible}
+            setVisible={setLeftVisible}
+            activeKey={leftActiveKey}
+            setActiveKey={setLeftActiveKey as any}
+          >
+            <CompositePanel.Item
+              title="flowDesigner.panels.resource"
+              icon="Add"
+            />
+          </CompositePanel>
+        </TopbarPanel.Region>
+        {/* <TopbarPanel.Region position="center">
+      <CompositePanel>
+        <CompositePanel.Item title="回退" onClick={back} icon="Undo" />
+        <CompositePanel.Item title="前进" onClick={next} icon="Redo" />
+      </CompositePanel>
+    </TopbarPanel.Region> */}
+        <TopbarPanel.Region position="right">
+          <CompositePanel
+            direction="right"
+            visible={rightVisible}
+            setVisible={setRightVisible}
+            activeKey={rightActiveKey}
+            setActiveKey={setRightActiveKey as any}
+          >
+            {/* <CompositePanel.Item
+          title="flowDesigner.panels.warn"
+          icon="Profile"
+          activeKey="warn"
+        /> */}
+            <CompositePanel.Item
+              title="flowDesigner.panels.error"
+              icon="Profile"
+              activeKey="error"
+            />
+          </CompositePanel>
+        </TopbarPanel.Region>
+      </TopbarPanel>
+      <div
+        style={{
+          display: 'flex',
+          flexGrow: 1,
+          height: 'calc(100% - 48px)',
+        }}
+      >
+        <CompositePanelContent
+          activeKey={leftActiveKey}
+          visible={leftVisible}
+          onClose={() => setLeftVisible(false)}
+        >
+          <CompositePanelContent.Item title="flowDesigner.panels.element">
+            {/* <ResourceWidget flowGraph={flowGraph} /> */}
+            {/* <LeftPanel /> */}
+            <ResourceWidget
+              title="flowDesigner.panels.sources.logical"
+              sources={itemMap}
+            />
+          </CompositePanelContent.Item>
+        </CompositePanelContent>
+        <WorkspacePanel>
+          {/* <FlowEditor /> */}
+          <FlowCanvas />
+        </WorkspacePanel>
+        <CompositePanelContent
+          visible={rightVisible}
+          activeKey={rightActiveKey}
+          onClose={() => setRightVisible(false)}
+        >
+          {/* <CompositePanelContent.Item
+        title="flowDesigner.panels.warn"
+        activeKey="warn"
+      >
+        <WarnWidget dataList={warnData} />
+      </CompositePanelContent.Item> */}
+          <CompositePanelContent.Item title="flowDesigner.panels.error">
+            <ErrorWidget dataList={errorData} />
+          </CompositePanelContent.Item>
+        </CompositePanelContent>
+      </div>
+    </StudioPanel>
   )
 }
