66848295742ec19f39ecb539976b749933460ccb
xf <575994260@qq.com>
2023-03-27 11:14:52 +0800

    fix: 公式

--- packages/flow-designable/src/nodes/FormulaModel.tsx
+++ packages/flow-designable/src/nodes/FormulaModel.tsx
@@ -2,8 +2,8 @@ import React, { FC, useState, CSSProperties, useRef, useEffect } from 'react'
 import { Modal, Button } from 'antd'
 import { IFieldMeta, MetaValueType } from '@toy-box/meta-schema'
 // import { MonacoInput } from '@toy-box/designable-react-settings-form'
-// import { PowerfxWorkerManager } from '@toy-box/powerfx-worker';
-// import { PowerFxFormulaEditor } from '@toy-box/powerfx-editor'
+import { PowerfxWorkerManager } from '@toy-box/powerfx-worker'
+import { PowerFxFormulaEditor } from '@toy-box/powerfx-editor'
 import { TextWidget } from '@toy-box/studio-base'
 import './index.less'
 import { IResourceMetaflow } from '../interface'
@@ -35,7 +35,7 @@ export const FormulaModel: FC<FormulaModelPorps> = ({
   const [formulaValue, setFormulaValue] = useState(value || '')
   const prefix = useFlowPrefix('-powerfx-editor')
   const variableMap: Record<string, IFieldMeta> = {}
-  // const [client, setClient] = useState<PowerfxWorkerManager>();
+  const [client, setClient] = useState<PowerfxWorkerManager>()
   const [context, setContext] = useState(
     JSON.stringify({ A: 'ABC', B: { Inner: 123 } })
   )
@@ -43,12 +43,12 @@ export const FormulaModel: FC<FormulaModelPorps> = ({
     setFormulaValue(res)
   }
 
-  // useEffect(() => {
-  //   setClient(new PowerfxWorkerManager());
-  //   return () => {
-  //     client?.dispose();
-  //   };
-  // }, []);
+  useEffect(() => {
+    setClient(new PowerfxWorkerManager())
+    return () => {
+      client?.dispose()
+    }
+  }, [])
 
   const showModal = () => {
     setIsModalVisible(true)
@@ -90,47 +90,47 @@ export const FormulaModel: FC<FormulaModelPorps> = ({
         onCancel={handleCancel}
       >
         {isModalVisible && (
-          <div>111111111</div>
+          // <div>111111111</div>
           // <MonacoInput
           //   value={formulaValue}
           //   onChange={callback}
           //   width={'100%'}
           //   height={'400px'}
           // />
-          //     <PowerFxFormulaEditor
-          //       style={{ border: '1px solid grey' }}
-          //       context={context}
-          //       defaultValue=""
-          //       minLineCount={1}
-          //       maxLineCount={6}
-          //       // width={200}
-          //       editorFocusOnMount={true}
-          //       monacoEditorOptions={{
-          //         fontSize: 14,
-          //         fixedOverflowWidgets: false,
-          //         minimap: {
-          //           enabled: false,
-          //         },
-          //       }}
-          //       onChange={(newValue: string): void => {
-          //         setFormulaValue(() => newValue);
-          //         // this._evalAsync(context, newValue);//计算自行处理
-          //       } }
-          //       onEditorDidMount={(editor, _): void => {
-          //         // this._editor = editor;
-          //       } }
-          //       lspConfig={{
-          //         disableDidOpenNotification: true,
-          //         enableSignatureHelpRequest: false,
-          //       }}
-          //       variables={['$User', 'Name']}
-          //       // metaData={metaData as any}
-          //       client={client}
-          //       metaData={{
-          //         dataView: undefined,
-          //         metaData: undefined
-          //       }}        // setupWorker={setupWorker}
-          // />
+          <PowerFxFormulaEditor
+            style={{ border: '1px solid grey' }}
+            context={context}
+            defaultValue=""
+            minLineCount={1}
+            maxLineCount={6}
+            // width={200}
+            editorFocusOnMount={true}
+            monacoEditorOptions={{
+              fontSize: 14,
+              fixedOverflowWidgets: false,
+              minimap: {
+                enabled: false,
+              },
+            }}
+            onChange={(newValue: string): void => {
+              setFormulaValue(() => newValue)
+              // this._evalAsync(context, newValue);//计算自行处理
+            }}
+            onEditorDidMount={(editor, _): void => {
+              // this._editor = editor;
+            }}
+            lspConfig={{
+              disableDidOpenNotification: true,
+              enableSignatureHelpRequest: false,
+            }}
+            variables={['$User', 'Name']}
+            // metaData={metaData as any}
+            client={client}
+            metaData={{
+              dataView: undefined,
+              metaData: undefined,
+            }} // setupWorker={setupWorker}
+          />
         )}
       </Modal>
     </>
